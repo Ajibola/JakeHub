c3580d6460b597863a1b601f3f407bd415ec8f94
Ajibola Aiye <ajibolaaiyedogbon@yahoo.com>
2017-08-12 04:02:56 +0100

    implement app scheduler with observables

--- app/src/main/java/com/jibola/app/jakehub/ui/repo/RepoPresenterImpl.java
+++ app/src/main/java/com/jibola/app/jakehub/ui/repo/RepoPresenterImpl.java
@@ -1,15 +1,15 @@
 package com.jibola.app.jakehub.ui.repo;
 
-import com.jibola.app.jakehub.app.JakeApplication;
+import com.jibola.app.jakehub.app.AppScheduler;
+import com.jibola.app.jakehub.app.SchedulerProvider;
 import com.jibola.app.jakehub.domain.model.Repo;
 
 import java.util.List;
 
 import javax.inject.Inject;
 
-import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
+import io.reactivex.functions.Function;
 
 /**
  * Created by hp on 8/11/2017.
@@ -20,11 +20,13 @@ public class RepoPresenterImpl implements RepoContract.Presenter {
     @Inject
     RepoContract.Interactor interactor;
 
+    @Inject
+    SchedulerProvider appScheduler;
+
     private RepoContract.View view;
 
     @Inject
     public RepoPresenterImpl() {
-
     }
 
     @Override
@@ -39,10 +41,19 @@ public class RepoPresenterImpl implements RepoContract.Presenter {
     }
 
     @Override
-    public void loadMoreRepositories(int page, final boolean refresh) {
-        interactor.loadRepositories(page, 15)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
+    public void loadMoreRepositories(final int page, final boolean refresh) {
+        interactor.loadRepositories(page)
+                .observeOn(appScheduler.mainThread())
+                .subscribeOn(appScheduler.backgroundThread())
+                .onErrorReturn(new Function<Throwable, List<Repo>>() {
+                    @Override
+                    public List<Repo> apply(Throwable throwable) throws Exception {
+                        view.hideLoading();
+                        List<Repo> localData = interactor.loadCachedRepositories(page).blockingFirst();
+                        view.showMessage(throwable);
+                        return localData;
+                    }
+                })
                 .subscribe(new Consumer<List<Repo>>() {
                     @Override
                     public void accept(List<Repo> repoList) throws Exception {
@@ -50,14 +61,8 @@ public class RepoPresenterImpl implements RepoContract.Presenter {
                         if (repoList != null && repoList.size() > 0) {
                             view.displayRepositories(repoList, refresh);
                         } else {
-                            view.showMessage("Unable to load item");
+                            view.showMessage(new Exception("No repos loaded"));
                         }
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) throws Exception {
-                        view.hideLoading();
-                        view.showMessage(throwable.getMessage());
                     }
                 });
     }

cf2ccc7e35cb128b578ded70a26a8e56ead9e81b
Ajibola Aiye <ajibolaaiyedogbon@yahoo.com>
2017-08-12 04:01:58 +0100

    changed endpoint, removed trailing slash

--- app/src/main/java/com/jibola/app/jakehub/data/network/GitHubApi.java
+++ app/src/main/java/com/jibola/app/jakehub/data/network/GitHubApi.java
@@ -17,7 +17,7 @@ public interface GitHubApi {
 
     public static final String API_ENDPOINT = "https://api.github.com/";
 
-    @GET("users/{user}/repos/")
+    @GET("users/{user}/repos")
     Observable<List<GitRepo>> getUserRepos(
             @Path("user") String user, @Query("page") int page, @Query
             ("per_page") int

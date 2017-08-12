42382706f1280fbe4b2a0d61bb409d0fb3a132ba
Ajibola Aiye <ajibolaaiyedogbon@yahoo.com>
2017-08-12 01:12:57 +0100

    real data source class and data object

---
+++ app/src/main/java/com/jibola/app/jakehub/data/local/LocalDataSource.java
@@ -0,0 +1,40 @@
+package com.jibola.app.jakehub.data.local;
+
+import com.jibola.app.jakehub.data.local.entity.GitRepoRealm;
+import com.jibola.app.jakehub.data.local.mapper.RepoRealmMapper;
+import com.jibola.app.jakehub.domain.DataSource;
+import com.jibola.app.jakehub.domain.model.Repo;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import io.reactivex.Observable;
+import io.realm.Realm;
+import io.realm.RealmResults;
+
+/**
+ * Created by hp on 8/10/2017.
+ */
+
+public class LocalDataSource implements DataSource {
+
+    @Inject
+    public LocalDataSource() {
+    }
+
+    @Override
+    public Observable<List<Repo>> getRepos(String userName, int page, int limit) {
+        int skipCount = page * limit;
+        int skipEnd = skipCount + limit;
+
+        Realm realm = Realm.getDefaultInstance();
+        RealmResults<GitRepoRealm> gitRepoRealms = realm.where(GitRepoRealm.class).findAll();
+
+        skipEnd = (skipEnd > gitRepoRealms.size()) ? gitRepoRealms.size() : skipEnd;
+        List<Repo> repoList = (List<Repo>) new RepoRealmMapper().mapObjectCollection(gitRepoRealms.subList(skipCount,
+                skipEnd));
+
+        return Observable.just(repoList);
+    }
+}
---
+++ app/src/main/java/com/jibola/app/jakehub/data/local/entity/GitRepoRealm.java
@@ -0,0 +1,94 @@
+package com.jibola.app.jakehub.data.local.entity;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by hp on 8/9/2017.
+ */
+
+public class GitRepoRealm extends RealmObject {
+
+    @PrimaryKey
+    private long id;
+    private String name;
+    private String full_name;
+    private String html_url;
+    private String description;
+    private String language;
+    private int watchers;
+    private int stars;
+    private int forks;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public String getHtml_url() {
+        return html_url;
+    }
+
+    public void setHtml_url(String html_url) {
+        this.html_url = html_url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public int getWatchers() {
+        return watchers;
+    }
+
+    public void setWatchers(int watchers) {
+        this.watchers = watchers;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
+
+    public int getForks() {
+        return forks;
+    }
+
+    public void setForks(int forks) {
+        this.forks = forks;
+    }
+}
